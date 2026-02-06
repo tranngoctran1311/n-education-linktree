09db3beb665d105606ee7dad5a1d83a2dcfcb74d
tranngoctran1311 <ngoctran.13112001@gmail.com>
2026-02-06 21:14:08 +0700

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => {
-  return (
-    <footer className="text-center py-8 text-gray-400 text-xs bg-white border-t border-gray-100">
-      <p>© 2026 N Education. All rights reserved.</p>
-    </footer>
-  );
-};
