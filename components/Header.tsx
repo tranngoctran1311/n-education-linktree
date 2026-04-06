d367328b14d8e2879a2b68b1256dec84b6ee7565
tranngoctran1311 <ngoctran.13112001@gmail.com>
2026-04-06 12:18:45 +0700

    Update image paths to relative references

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
       <div className="h-48 overflow-hidden relative">
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/10 z-10"></div>
         <img 
-          src="/Banner_bio.png" 
+          src="./Banner_bio.png" 
           alt="N Education Banner" 
           className="w-full h-full object-cover"
         />
@@ -15,7 +15,7 @@ export const Header: React.FC = () => {
       <div className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 z-20">
         <div className="w-32 h-32 rounded-full border-[6px] border-white bg-white shadow-lg overflow-hidden flex items-center justify-center bg-brand-gold">
           <img 
-            src="/logo_nedu.png" 
+            src="./logo_nedu.png" 
             alt="N Education Logo" 
             className="w-full h-full object-cover"
           />
